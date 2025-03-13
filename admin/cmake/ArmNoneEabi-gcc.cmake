cd45d7eef17f1eeb6fae82ce8b585eaec246f9c3
Chiang, Yi <yi.chiang@bmw.de>
2025-03-13 09:23:56 +0100

    move gcc-specific parts from ArmNoneEabi.cmake to ArmNoneEabi-gcc.cmake
    Signed-off-by: Chiang, Yi <yi.chiang@bmw.de>

---
+++ admin/cmake/ArmNoneEabi-gcc.cmake
@@ -0,0 +1,15 @@
+# Toolchain for gcc-arm-none-eabi-10.3-2021.10
+include("${CMAKE_CURRENT_LIST_DIR}/ArmNoneEabi.cmake")
+
+set(C_COMMON_FLAGS "-funsigned-bitfields -femit-class-debug-always")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${C_COMMON_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${C_COMMON_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} \
+--specs=nano.specs -specs=nosys.specs")
+
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER arm-none-eabi-as)
+set(CMAKE_LINKER arm-none-eabi-g++)
+set(CMAKE_AR arm-none-eabi-ar)
